388aa733e2edbf2840124653bf20e9a75ad5a619
Kavan Vavadiya <vavadiya.kavan@grofers.com>
2026-06-07 22:50:00 +0530

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.mp3', '**/*.wav', '**/*.ogg', '**/*.m4a'],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
